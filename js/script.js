8a60792f76b1015d9483d2eca74967b893b1fa36
Yuyi50 <yuyisantiago50@gmail.com>
2020-09-21 22:25:20 -0500

    Among Us

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function ispaAmong() {
+    let impostor= true;
+
+if (impostor == !true){
+    alert("You are Crewmate")
+} else {
+    alert("You are Impostor")
+}
+}
+function ispaAmong2() {
+   
+    let impostor= true;
+
+    if (impostor == !true){
+        alert("You are Impostor")
+    } else {
+        alert("You are Crewmate")
+    }
+
+}
